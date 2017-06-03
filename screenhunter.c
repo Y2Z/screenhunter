b1baa8ed2bb8970afdc189374ac790833e76a504
vflyson <vflyson@users.noreply.github.com>
2017-06-03 12:46:09 -0400

    Add verbose (-v) flag, change -h to -k, refactor error messages

--- screenhunter.c
+++ screenhunter.c
@@ -12,31 +12,35 @@
 #include <png.h>
 
 
+#define PROGNAME "screenhunter"
+
+
 typedef unsigned char uchar;
 typedef unsigned int uint;
 typedef unsigned long ulong;
 typedef unsigned short ushort;
 
 typedef struct {
-    char *name;       /* base file name */
-    ushort colors;    /* colors per pixel */
-    uint width;       /* width in pixels */
-    uint height;      /* height in pixels */
-    png_byte type;    /* png color type */
-    png_infop info;   /* png info struct*/
-    png_bytep *rows;  /* png row pointers */
-    png_structp png;  /* png struct pointer */
+    char *filename;         /* base file name */
+    uint width;             /* width in pixels */
+    uint height;            /* height in pixels */
+    ushort colors;          /* colors per pixel */
+    png_byte color_type;    /* png color type */
+    png_infop info_ptr;     /* png info struct*/
+    png_structp png_ptr;    /* png struct pointer */
+    png_bytep *image_data;  /* png row pointers */
 } Target;
 
 
 /* Default options' values */
-char optDontClick = 0;
+char optJustScan = 0;
 char optOneMatch = 0;
-char optHoldCursor = 0;
+char optKeepPosition = 0;
 char optRandom = 0;
+char optVerbose = 0;
 
 
-void msleep(int milliseconds)
+void msleep(const uint milliseconds)
 {
     struct timespec ts;
 
@@ -46,7 +50,7 @@ void msleep(int milliseconds)
     nanosleep(&ts, NULL);
 }
 
-int randr(int min, int max)
+int randr(const int min, const int max)
 {
     return rand() % (max + 1 - min) + min;
 }
@@ -62,7 +66,7 @@ void aim(Display *display, Window *window, uint x1, uint y1, uint x2, uint y2)
     }
 }
 
-void click(Display *display, Window *window, int button)
+void click(Display *display, Window *window, const ushort button)
 {
     XEvent event;
 
@@ -87,8 +91,9 @@ void click(Display *display, Window *window, int button)
     }
 
     if (XSendEvent(display, PointerWindow, True, 0xfff, &event) == 0) {
-        fprintf(stderr, "Error on mousedown\n");
+        fprintf(stderr, "%s: mousedown\n", PROGNAME);
     }
+
     XFlush(display);
 
     msleep((optRandom) ? randr(21, 99) : 30);
@@ -97,109 +102,131 @@ void click(Display *display, Window *window, int button)
     event.xbutton.state = 0x100;
 
     if (XSendEvent(display, PointerWindow, True, 0xfff, &event) == 0) {
-        fprintf(stderr, "Error on mouseup\n");
+        fprintf(stderr, "%s: mouseup\n", PROGNAME);
     }
+
     XFlush(display);
 }
 
-char seekandclick(char *file_name, Display *display, Window window, XImage *screenshot)
+int seekandclick(char *filename, Display *display,
+                  Window window, XImage *screenshot)
 {
-    FILE *fp;
+    FILE *file_ptr;
     Target target;
     uchar header[8];
     Window root, child;
     int root_x, root_y, win_x, win_y;
     uint mask;
-    char res = 0;
+    int res = 0;
 
-    target.name = basename(file_name);
+    target.filename = basename(filename);
 
     /* Open file */
-    if (!(fp = fopen(file_name, "rb"))) {
-        fprintf(stderr, "%s: could not be opened for reading", target.name);
+    if (!(file_ptr = fopen(filename, "rb"))) {
+        fprintf(stderr, "%s: file '%s' could not be opened for reading\n",
+                PROGNAME, target.filename);
         res = -1;
         goto exit;
     }
 
-    fread(header, 1, 8, fp);
+    fread(header, 1, 8, file_ptr);
     if (png_sig_cmp(header, 0, 8)) {
-        fprintf(stderr, "%s: not a PNG file", target.name);
+        fprintf(stderr, "%s: '%s' is not a valid PNG file\n",
+                PROGNAME, target.filename);
         res = -1;
-        goto exit;
+        goto fclose_and_exit;
     }
 
-    /* Initialize stuff */
-    target.png = png_create_read_struct(PNG_LIBPNG_VER_STRING, NULL, NULL, NULL);
-
-    if (!target.png) {
-        fprintf(stderr, "%s: png_create_read_struct failed", target.name);
+    /* Initialize an instance of libpng read structure */
+    target.png_ptr = png_create_read_struct(PNG_LIBPNG_VER_STRING, NULL, NULL,
+                                            NULL);
+    if (!target.png_ptr) {
+        fprintf(stderr, "%s: unable to process file '%s'"
+                        "(png_create_read_struct failed)\n",
+                PROGNAME, target.filename);
         res = -1;
-        goto exit;
+        goto free_read_struct_and_exit;
     }
 
-    target.info = png_create_info_struct(target.png);
-    if (!target.info) {
-        fprintf(stderr, "%s: png_create_info_struct failed", target.name);
+    target.info_ptr = png_create_info_struct(target.png_ptr);
+    if (!target.info_ptr) {
+        fprintf(stderr, "%s: unable to process file '%s'"
+                        "(png_create_info_struct failed)\n",
+                PROGNAME, target.filename);
         res = -1;
-        goto exit;
+        goto free_read_struct_and_exit;
     }
 
-    if (setjmp(png_jmpbuf(target.png))) {
-        fprintf(stderr, "%s: error during init_io", target.name);
+    if (setjmp(png_jmpbuf(target.png_ptr))) {
+        fprintf(stderr, "%s: unable to process file '%s' (init_io failed)\n",
+                PROGNAME, target.filename);
         res = -1;
-        goto exit;
-    }
-
-    png_init_io(target.png, fp);
-    png_set_sig_bytes(target.png, 8);
-
-    png_read_info(target.png, target.info);
-
-    target.type = png_get_color_type(target.png, target.info);
-
-    if (target.type == PNG_COLOR_TYPE_RGB) {
-        target.colors = 3;
-    } else if (target.type == PNG_COLOR_TYPE_RGBA) {
-        target.colors = 4;
-        /* Give a warning about non-opaque pixels */
-        fprintf(stderr, "%s: alpha-channel found, screenhunter will treat all non-opaque pixels as positive matches\n", target.name);
+        goto free_read_struct_and_exit;
     } else {
-        fprintf(stderr, "%s: color type of input file must be PNG_COLOR_TYPE_RGB (%d)"
-                " or PNG_COLOR_TYPE_RGBA (%d)"
-                ", (detected %d)\n",
-                target.name,
-                PNG_COLOR_TYPE_RGB,
-                PNG_COLOR_TYPE_RGBA,
-                target.type);
-        res = -1;
-        goto exit;
+        res = 0;
     }
 
-    if (png_get_bit_depth(target.png, target.info) != 8) {
-        fprintf(stderr, "%s: incorrect bit depth of %d (expected 8)\n", target.name, png_get_bit_depth(target.png, target.info));
+    png_init_io(target.png_ptr, file_ptr);
+    png_set_sig_bytes(target.png_ptr, 8);
+
+    png_read_info(target.png_ptr, target.info_ptr);
+
+    target.color_type = png_get_color_type(target.png_ptr, target.info_ptr);
+    if (target.color_type == PNG_COLOR_TYPE_RGB) {
+        target.colors = 3;
+    } else if (target.color_type == PNG_COLOR_TYPE_RGBA) {
+        target.colors = 4;
+        if (optVerbose) {
+            /* Give a warning about non-opaque pixels */
+            fprintf(stderr, "%s: file '%s' has an alpha channel, "
+                            "all non-opaque pixels "
+                            "will be treated as positive matches\n",
+                    PROGNAME, target.filename);
+        }
+    } else {
+        fprintf(stderr, "%s: the required color type is either "
+                        "PNG_COLOR_TYPE_RGB (%d) or PNG_COLOR_TYPE_RGBA (%d), "
+                        "the input file '%s' has unsupported color type (%d)\n",
+                PROGNAME,
+                PNG_COLOR_TYPE_RGB, PNG_COLOR_TYPE_RGBA,
+                target.filename, target.color_type);
         res = -1;
-        goto exit;
+        goto free_read_struct_and_exit;
+    }
+
+    if (png_get_bit_depth(target.png_ptr, target.info_ptr) != 8) {
+        fprintf(stderr, "%s: file '%s' has bit depth of %d (expected 8)\n",
+                PROGNAME, target.filename,
+                png_get_bit_depth(target.png_ptr, target.info_ptr));
+        res = -1;
+        goto free_read_struct_and_exit;
+    }
+
+    target.width = png_get_image_width(target.png_ptr, target.info_ptr);
+    target.height = png_get_image_height(target.png_ptr, target.info_ptr);
+
+    target.image_data = (png_bytep*)malloc(sizeof(png_bytep) * target.height);
+    for (uint target_y = 0 ; target_y < target.height ; target_y++) {
+        target.image_data[target_y] = (png_byte*)malloc(
+            png_get_rowbytes(target.png_ptr, target.info_ptr)
+        );
     }
 
     /* Read file */
-    if (setjmp(png_jmpbuf(target.png))) {
-        fprintf(stderr, "%s: error during read_image", target.name);
+    if (setjmp(png_jmpbuf(target.png_ptr))) {
+        fprintf(stderr, "%s: unable to perform read_image on file '%s'\n",
+                PROGNAME, target.filename);
         res = -1;
-        goto exit;
+        goto free_rows_pointers_and_exit;
+    } else {
+        res = 0;
     }
 
-    target.width = png_get_image_width(target.png, target.info);
-    target.height = png_get_image_height(target.png, target.info);
-
-    target.rows = (png_bytep*)malloc(sizeof(png_bytep) * target.height);
-    for (uint target_y = 0 ; target_y < target.height ; target_y++) {
-        target.rows[target_y] = (png_byte*)malloc(png_get_rowbytes(target.png, target.info));
-    }
-
-    png_read_image(target.png, target.rows);
+    png_read_image(target.png_ptr, target.image_data);
 
     /* Get current cursor position */
-    XQueryPointer(display, window, &root, &child, &root_x, &root_y, &win_x, &win_y, &mask);
+    XQueryPointer(display, window, &root, &child,
+                  &root_x, &root_y, &win_x, &win_y, &mask);
 
 /*
  *
@@ -216,24 +243,24 @@ char seekandclick(char *file_name, Display *display, Window window, XImage *scre
     uchar *screenshot_pixel;
     uchar target_has_alpha = target.colors - 3;
     ulong target_amount_of_pixels = target.width * target.height;
-    ulong so_far_so_good = 0;
-    ulong found = 0;
+    ulong ok = 0;
+    ulong matches = 0;
 
     for (uint y = 0 ; y < screenshot->height - target.height ; y++) {
         for (uint x = 0 ; x < screenshot->width - target.width ; x++) {
             screenshot_pixel = (uchar*)&(screenshot->data[screenshot->bytes_per_line * y + screenshot->bits_per_pixel * x / NBBY]);
 
-            if ((target_has_alpha && (*target.rows)[3] < 255) ||
-                (screenshot_pixel[2] == (*target.rows)[0] &&
-                 screenshot_pixel[1] == (*target.rows)[1] &&
-                 screenshot_pixel[0] == (*target.rows)[2])
+            if ((target_has_alpha && (*target.image_data)[3] < 255) ||
+                (screenshot_pixel[2] == (*target.image_data)[0] &&
+                 screenshot_pixel[1] == (*target.image_data)[1] &&
+                 screenshot_pixel[0] == (*target.image_data)[2])
             ) {
-                so_far_so_good = 1;
+                ok = 1;
 
-                for (uint ty = 0 ; ty < target.height && so_far_so_good ; ty++) {
-                    target_row = target.rows[ty];
+                for (uint ty = 0 ; ty < target.height && ok ; ty++) {
+                    target_row = target.image_data[ty];
 
-                    for (uint tx = 0 ; tx < target.width && so_far_so_good ; tx++) {
+                    for (uint tx = 0 ; tx < target.width && ok ; tx++) {
                         target_pixel = &(target_row[tx * target.colors]);
 
                         screenshot_pixel = (uchar*)&(screenshot->data[screenshot->bytes_per_line * (y+ty) + screenshot->bits_per_pixel * (x+tx) / NBBY]);
@@ -243,22 +270,24 @@ char seekandclick(char *file_name, Display *display, Window window, XImage *scre
                              screenshot_pixel[1] == target_pixel[1] &&
                              screenshot_pixel[0] == target_pixel[2])
                         ) {
-                            so_far_so_good++;
+                            ok++;
 
-                            if (so_far_so_good == target_amount_of_pixels) {
-                                found++;
+                            if (ok == target_amount_of_pixels) {
+                                matches++;
 
-                                if (!optDontClick) {
-                                    aim(display, &window, x, y, x + target.width, y + target.height);
+                                if (!optJustScan) {
+                                    aim(display, &window, x, y,
+                                        x + target.width, y + target.height);
                                     click(display, &window, Button1);
                                 }
 
                                 if (optOneMatch) {
-                                    y = screenshot->height - target.height; /* break out of the topmost loop */
+                                    /* break out of the topmost loop */
+                                    y = screenshot->height - target.height;
                                 }
                             }
                         } else {
-                            so_far_so_good = 0;
+                            ok = 0;
                         }
                     }
                 }
@@ -266,63 +295,79 @@ char seekandclick(char *file_name, Display *display, Window window, XImage *scre
         }
     }
 
-    if (found) {
+    if (matches) {
         res = 1;
-
-        if (!optDontClick && !optHoldCursor) {
+        if (!optJustScan && !optKeepPosition) {
             /* Return cursor to its original position */
             aim(display, &window, root_x, root_y, root_x, root_y);
         }
     }
 
-    /* Clean-up */
+free_rows_pointers_and_exit:
     for (uint j = 0 ; j < target.height ; j++) {
-        free(target.rows[j]);
+        free(target.image_data[j]);
     }
-    free(target.rows);
-
+    free(target.image_data);
+free_read_struct_and_exit:
+    png_destroy_read_struct(&target.png_ptr, &target.info_ptr, NULL);
+fclose_and_exit:
+    fclose(file_ptr);
 exit:
-    png_destroy_read_struct(&target.png, &target.info, NULL);
-    fclose(fp);
-
     return res;
+}
+
+void print_usage()
+{
+    fprintf(stderr, "Usage: %s [-hvsokr] target_image.png\n", PROGNAME);
 }
 
 int main(int argc, char **argv)
 {
-    char res = 0;
+    int res = 0;
     int opt;
+
+    if (argc < 2) {
+        fprintf(stderr, "%s: input file required\n", PROGNAME);
+        print_usage();
+        res = -1;
+        goto exit;
+    }
+
+    while ((opt = getopt(argc, argv, "hvsokr")) != -1)
+    {
+        switch (opt)
+        {
+            case 'v': optVerbose++; break;
+            case 's': optJustScan++; break;
+            case 'o': optOneMatch++; break;
+            case 'k': optKeepPosition++; break;
+            case 'r': optRandom++; break;
+
+            case 'h':
+                print_usage();
+                res = 0;
+                goto exit;
+
+            case '?':
+                fprintf(stderr, "%s: Unrecognized option: -%c\n",
+                        PROGNAME, optopt);
+                print_usage();
+                res = -1;
+                goto exit;
+        }
+    }
 
     Display *display = XOpenDisplay(0);
 
     if (!display) {
-        fprintf(stderr, "Error opening display\n");
+        fprintf(stderr, "%s: unable to open display :%d\n", PROGNAME, 0);
         res = -1;
         goto free_display_and_exit;
     }
 
-    while ((opt = getopt(argc, argv, "sohr")) != -1)
-    {
-        switch (opt)
-        {
-            case 's': optDontClick = 1; break;
-            case 'o': optOneMatch = 1; break;
-            case 'h': optHoldCursor = 1; break;
-            case 'r': optRandom = 1; break;
-
-            default:
-                /* No arguments given */
-                fprintf(stderr, "Usage: %s [-sohr] target.png\n", argv[0]);
-                res = -1;
-                goto free_display_and_exit;
-        }
-    }
-
     Window window = DefaultRootWindow(display);
     XWindowAttributes windowAttrs;
-
     XGetWindowAttributes(display, window, &windowAttrs);
-
     XImage *screenshot = XGetImage(display, window, 0, 0,
                            windowAttrs.width, windowAttrs.height,
                            AllPlanes, ZPixmap);
@@ -331,11 +376,14 @@ int main(int argc, char **argv)
         srand(time(NULL));
     }
 
-    //printf("%d (%d)\n", optind, argc);
     if (optind < argc) {
-        do {
+        //do {
             res = seekandclick(argv[optind], display, window, screenshot);
-        } while (++optind < argc);
+        //} while (++optind < argc);
+    } else {
+        fprintf(stderr, "%s: input file required\n", PROGNAME);
+        print_usage(argv[0]);
+        res = -1;
     }
 
 //free_everything_and_exit:
@@ -343,6 +391,6 @@ int main(int argc, char **argv)
 free_display_and_exit:
     XFlush(display);
     XCloseDisplay(display);
-//exit:
+exit:
     return res;
 }
